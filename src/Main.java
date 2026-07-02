ed77153ae0e00311b3b90f10b2e2bb43b23e6c39
enrurizkysaputra-tech <enrurizkysaputra@gmail.com>
2026-07-02 18:17:33 +0800

    Project UAS PBO1 - RentGo Indonesia - Enru Rizky Saputra

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+// Project UAS PBO1 - RentGo Indonesia
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
